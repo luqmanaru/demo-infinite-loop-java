6bf0fe80141fad4172b2ecaa4fe2bc8ec39f5ae2
Hanif Luqmanul Hakim <hanifluqmanulhakim20@gmail.com>
2025-05-20 14:14:35 +0700

    Create demo-infinite-loop.java

---
+++ demo-infinite-loop.java
@@ -0,0 +1,13 @@
+/**
+ * Demonstrasi Infinite While Loop
+ * dengan pesan positif untuk ibu
+ * @author luqmanaru
+ */
+public class InfiniteLove {
+    public static void main(String[] args) { 
+        // Infinite loop dengan pesan spesial
+        while(true) {
+            System.out.println("IBU CANTIK DEH HARI INI<3"); 
+        }
+    }
+}
